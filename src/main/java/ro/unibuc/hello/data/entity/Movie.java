82af01014e6a3619ed53bfa06604800c1f8eedc1
Tudor Coman <tudor.coman@s.unibuc.ro>
2024-04-03 03:08:11 +0300

    Add integration tests for Actor, Movie, Role and E2E scenario (#7)

--- src/main/java/ro/unibuc/hello/data/entity/Movie.java
+++ src/main/java/ro/unibuc/hello/data/entity/Movie.java
@@ -1,5 +1,6 @@
 package ro.unibuc.hello.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +18,30 @@ import java.util.List;
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @EqualsAndHashCode
 public class Movie {
-    @Id private String id;
-    @NotNull private String title;
-    @NotNull private String description;
+    @Id
+    @JsonProperty("id")
+    private String id;
+
+    @NotNull
+    @JsonProperty("title")
+    private String title;
+
+    @NotNull
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("popularity")
     private Integer popularity;
+
+    @JsonProperty("genres")
     private List<String> genres;
+
+    @JsonProperty("tmdbId")
     private Long tmdbId;
+
+    @JsonProperty("year")
     private Long year;
 }
